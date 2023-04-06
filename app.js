307a953f69cff96899c2345c8386cc1cd60141ec
Sai Krishna P <123139223+s558962@users.noreply.github.com>
2023-04-05 21:22:13 -0500

    changed

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var bagsRouter = require('./routes/bags');
 var boardRouter = require('./routes/board');
-//var selectorRouter = require('./routes/selector');
+var selectorRouter = require('./routes/selector');
 
 var app = express();
 
@@ -26,7 +26,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/bags', bagsRouter);
 app.use('/board', boardRouter);
-//app.use('/selector', selectorRouter);
+app.use('/selector', selectorRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
